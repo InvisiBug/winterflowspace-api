8bd05f719eb5ef2aaaee64817c98ad58213bb70a
InvisiBug <matthewjkavanagh@gmail.com>
2026-02-05 06:03:47 +0000

    temp import fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { json } from "express";
 import cors from "cors";
-import { getStudioBookings } from "./lib/utils";
-
+import { Booking } from "srclib/types";
 const app = express();
 
 const PORT = 3000;
@@ -151,3 +150,49 @@ app.get("/health", (_, res) => {
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT} 📡`);
 });
+
+// dunno why this isnt working with the import
+export const getStudioBookings = async (data: any): Promise<Booking[][]> => {
+  const numDays = 2;
+  const studioBookings: Booking[][] = [];
+  const activities = data.activities;
+
+  Array.from({ length: numDays }).forEach((_, dayIndex: number) => {
+    const dailyStudioBookings = new Array<Booking>();
+
+    // activities.forEach((activity: any) => {
+    for (const activity of activities) {
+      if (activity.studio.toUpperCase() === "STUDIO") {
+        // Check if activity is today
+        const activityDate = new Date(activity.startDateTime.dateTime).toDateString();
+
+        const d = new Date();
+        const now = new Date(d.setDate(d.getDate() + dayIndex)).toDateString();
+
+        if (activityDate == now) {
+          const rawStartTime = activity.startDateTime.dateTime.replace("T", " "); // They keep changing the date format so pulled it out here
+
+          const startTime = new Date(rawStartTime);
+          const endTime = new Date(startTime.getTime() + activity.duration * 60000);
+
+          const startHours = String(startTime.getHours()).padStart(2, "0");
+          const startMinutes = String(startTime.getMinutes()).padStart(2, "0");
+
+          const endHours = String(endTime.getHours()).padStart(2, "0");
+          const endMinutes = String(endTime.getMinutes()).padStart(2, "0");
+
+          dailyStudioBookings.push({
+            start: `${startHours}:${startMinutes}`,
+            free: false,
+            end: `${endHours}:${endMinutes}`,
+          });
+        }
+      }
+    }
+    // console.log("🚀 ~ getStudioFreeTime ~ studioBookings:", studioBookings);
+    studioBookings.push(dailyStudioBookings);
+  });
+
+  console.log("🚀 ~ getStudioBookings ~ studioBookings:", studioBookings[1]);
+  return studioBookings;
+};
