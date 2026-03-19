f60bb71cded033620b67a29556a0eec606fc59f9
InvisiBug <matthewjkavanagh@gmail.com>
2026-03-19 10:41:23 +0000

    added fix for outdoor running club not having a studio

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,30 +8,32 @@ export const getStudioBookings = async (data: GetBookingsAPI): Promise<Booking[]
     const dailyStudioBookings = new Array<Booking>();
 
     for (const activity of data.activities) {
-      if (activity.studio.toUpperCase() === "STUDIO") {
-        // Check if activity is today
-        const activityDate = new Date(activity.startDateTime.dateTime).toDateString();
+      if (activity.studio !== undefined) {
+        if (activity.studio.toUpperCase() === "STUDIO") {
+          // Check if activity is today
+          const activityDate = new Date(activity.startDateTime.dateTime).toDateString();
 
-        const d = new Date();
-        const now = new Date(d.setDate(d.getDate() + dayIndex)).toDateString();
+          const d = new Date();
+          const now = new Date(d.setDate(d.getDate() + dayIndex)).toDateString();
 
-        if (activityDate == now) {
-          const rawStartTime = activity.startDateTime.dateTime.replace("T", " "); // They keep changing the date format so pulled it out here
+          if (activityDate == now) {
+            const rawStartTime = activity.startDateTime.dateTime.replace("T", " "); // They keep changing the date format so pulled it out here
 
-          const startTime = new Date(rawStartTime);
-          const endTime = new Date(startTime.getTime() + activity.duration * 60000);
+            const startTime = new Date(rawStartTime);
+            const endTime = new Date(startTime.getTime() + activity.duration * 60000);
 
-          const startHours = String(startTime.getHours()).padStart(2, "0");
-          const startMinutes = String(startTime.getMinutes()).padStart(2, "0");
+            const startHours = String(startTime.getHours()).padStart(2, "0");
+            const startMinutes = String(startTime.getMinutes()).padStart(2, "0");
 
-          const endHours = String(endTime.getHours()).padStart(2, "0");
-          const endMinutes = String(endTime.getMinutes()).padStart(2, "0");
+            const endHours = String(endTime.getHours()).padStart(2, "0");
+            const endMinutes = String(endTime.getMinutes()).padStart(2, "0");
 
-          dailyStudioBookings.push({
-            start: `${startHours}:${startMinutes}`,
-            free: false,
-            end: `${endHours}:${endMinutes}`,
-          });
+            dailyStudioBookings.push({
+              start: `${startHours}:${startMinutes}`,
+              free: false,
+              end: `${endHours}:${endMinutes}`,
+            });
+          }
         }
       }
     }
